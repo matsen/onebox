1074ce8fc6080fe061d2f3fbac0c1fccee486665
Erick Matsen <matsen@fhcrc.org>
2014-02-19 19:54:04 -0800

    Improving spec.

--- spec/lib/onebox/engine/pubmed_onebox_spec.rb
+++ spec/lib/onebox/engine/pubmed_onebox_spec.rb
@@ -2,29 +2,28 @@ require "spec_helper"
 
 describe Onebox::Engine::PubmedOnebox do
 
-  let(:link) { "http://www.ncbi.nlm.nih.gov/pubmed/7288891" }
-  let(:html) { described_class.new(link).to_html }
-
-  before do
-    fake(link, response("name.response"))
+  before(:all) do
+    @link = "http://www.ncbi.nlm.nih.gov/pubmed/7288891"
   end
 
-  it "has the paper's title" do
-    expect(html).to include("Evolutionary trees from DNA sequences: a maximum likelihood approach.")
-  end
+  shared_examples_for "#to_html" do
+    it "has the paper's title" do
+      expect(html).to include("Evolutionary trees from DNA sequences: a maximum likelihood approach.")
+    end
 
-  it "has the paper's author" do
-    expect(html).to include("Felsenstein")
-  end
+    it "has the paper's author" do
+      expect(html).to include("Felsenstein")
+    end
 
-  it "has the paper's abstract" do
-    expect(html).to include("The application of maximum likelihood techniques to the estimation of evolutionary trees from nucleic acid sequence data is discussed.") end
+    it "has the paper's abstract" do
+      expect(html).to include("The application of maximum likelihood techniques to the estimation of evolutionary trees from nucleic acid sequence data is discussed.") end
 
-  it "has the paper's date" do
-    expect(html).to include("1981")
-  end
+    it "has the paper's date" do
+      expect(html).to include("1981")
+    end
 
-  it "has the URL to the resource" do
-    expect(html).to include(link)
+    it "has the URL to the resource" do
+      expect(html).to include(link)
+    end
   end
 end
